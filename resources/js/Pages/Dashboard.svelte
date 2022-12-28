5311d3320ff0b42efedd8df0f6df9174825996b3
Kim Hallberg <hallberg.kim@gmail.com>
2022-12-28 20:59:17 +0100

    Fix broken header slot

--- resources/js/Pages/Dashboard.svelte
+++ resources/js/Pages/Dashboard.svelte
@@ -1,18 +1,17 @@
 <script context="module">
     import Layout from '@/Layouts/AuthenticatedLayout.svelte';
-    export const layout = Layout;
 </script>
 
-<slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        Broken header
+<Layout>
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight" slot="header">
+        Dashboard
     </h2>
-</slot>
 
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+            </div>
         </div>
     </div>
-</div>
+</Layout>
