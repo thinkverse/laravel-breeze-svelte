4eb63f4e8223fa52b564953e0855fb11f653c5ee
Kim Hallberg <hallberg.kim@gmail.com>
2022-12-28 03:47:46 +0100

    Convert dashboard to Svelte First pass

--- resources/js/Layouts/AuthenticatedLayout.svelte
+++ resources/js/Layouts/AuthenticatedLayout.svelte
@@ -1,16 +1,41 @@
 <script>
-    import ApplicationLogo from "@/Components/ApplicationLogo.svelte";
     import { Link } from "@inertiajs/inertia-svelte";
+    import { route } from "@/utils";
+    import ApplicationLogo from "@/Components/ApplicationLogo.svelte";
+    import NavLink from "@/Components/NavLink.svelte";
 </script>
 
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-    <div>
-        <Link href="/">
-            <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-        </Link>
-    </div>
+<div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <div class="shrink-0 flex items-center">
+                        <Link href="/">
+                            <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        </Link>
+                    </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <NavLink href={route('dashboard')} active={route().current('dashboard')}>
+                            Dashboard
+                        </NavLink>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </nav>
+
+    {#if $$slots.header}
+        <header class="bg-white dark:bg-gray-800 shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <slot name="header" />
+            </div>
+        </header>
+    {/if}
+
+    <main>
         <slot />
-    </div>
+    </main>
 </div>
--- resources/js/Pages/Dashboard.svelte
+++ resources/js/Pages/Dashboard.svelte
@@ -1,7 +1,13 @@
 <script context="module">
-    import Layout from '@/Layouts/GuestLayout.svelte';
+    import Layout from '@/Layouts/AuthenticatedLayout.svelte';
     export const layout = Layout;
 </script>
+
+<slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        Broken header
+    </h2>
+</slot>
 
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
